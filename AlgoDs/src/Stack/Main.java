1e90d98d03e1e1f2c21185247d974a0b0094fe07
Gustavo Teleguario <Maynor.Teleguario@octavia.de>
2020-04-09 23:12:23 +0200

    ADT Stack Array-form with Integer values.

--- AlgoDs/src/Stack/Main.java
+++ AlgoDs/src/Stack/Main.java
@@ -1,7 +1,25 @@
 package Stack;
 
+import java.util.Stack;
+
 public class Main {
-    public static void main(String...args){
+    public static void main(String... args) throws MyException {
+
+        // Example for Stack
+        StackArray stack = new StackArray();
+        System.out.println("Is Stack Empty? " + stack.empty());
+        System.out.println("Stack size: " + stack.getSize());
+        stack.push(34);
+        stack.push(3);
+        stack.push(88);
+        stack.push(90);
+        System.out.println(stack.toString());
+        System.out.println("Remove element LIFO term");
+        stack.pop();
+        System.out.println(stack.toString());
+        System.out.println("Element at the Top: " + stack.peek());
+        System.out.println(stack.toString());
+        System.out.println("Search Elemetn: " + stack.search(3));
 
     }
 }
--- AlgoDs/src/Stack/StackArray.java
+++ AlgoDs/src/Stack/StackArray.java
@@ -1,5 +1,101 @@
 package Stack;
 
+import java.lang.reflect.Array;
+
 public class StackArray {
 
+    /**
+     * Stack is a Dynamic ADT is based in the term LIFO
+     * (Last in First out)
+     */
+    static final int DEFAULT_SIZE = 10;
+    private int[] elements;
+    private int index;
+
+    public StackArray() {
+        this.elements = new int[DEFAULT_SIZE];
+        this.index = 0;
+    }
+
+    /**
+     * Pushes a Key onto the top of this Stack.
+     *
+     * @param key
+     * @throws MyException: if element add at index
+     *                      over the default size
+     */
+    public void push(int key) throws MyException {
+        if (index > elements.length)
+            throw new MyException();
+
+        elements[index] = key;
+        index++;
+    }
+
+    /**
+     * Looks at the key at the top of this stack
+     * without removing it from the stack.
+     * @return
+     * @throws MyException
+     */
+    public int peek() throws MyException {
+        int len = index - 1;
+        if (index == 0) {
+            throw new MyException();
+        }
+        return elements[len];
+    }
+
+    // Removes the object at the top of this stack
+    public void pop() throws MyException {
+        //int keyElement = peek();
+        int len = index - 1;
+        if (index == 0) {
+            throw new MyException();
+        }
+        elements[len] = 0;
+        index--;
+    }
+
+
+    // Print all elements into the Stack
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        for (int i = 0; i < index; i++) {
+            sb.append(elements[i]);
+            if (i < index - 1) sb.append(",");
+        }
+        sb.append(" ]");
+        return sb.toString();
+    }
+
+    // Tests if this stack is empty.
+    public boolean empty() {
+        return index == 0 ? true : false;
+    }
+
+    /**
+     * if key is into the Stack, this return this element, but not the
+     * index, if the element not exist into the Stack, its return a 0 value.
+     *
+     * @param key
+     * @return
+     * @throws MyException
+     */
+    public int search(int key) throws MyException {
+        if (index == 0) {
+            throw new MyException();
+        }
+        int ob = 0;
+        for (int i = 0; i < elements.length; i++) {
+            if (key == elements[i])
+                ob = elements[i];
+        }
+        return ob;
+    }
+
+    public int getSize() {
+        return this.index;
+    }
 }
